dab9c44b79298f8bf208ff3eecacc51ee9c26c06
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2026-07-29 16:02:18 +0500

    добавлен токенайзер

--- training/pipeline.py
+++ training/pipeline.py
@@ -6,41 +6,62 @@ from training.dataset_feature_extractor import DatasetFeatureExtractor
 
 from lion_pytorch import Lion
 from datasets import load_from_disk
-from transformers import WhisperFeatureExtractor, WhisperTokenizer
+from transformers import WhisperFeatureExtractor, WhisperTokenizer, PreTrainedTokenizerFast
 import torch
+
+WHISPER_MODEL_NAME = "openai/whisper-tiny"
+DATASET_PATH = "preprocessed_dataset/whisper_prepocessed_dataset.arrow"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-dataset = load_from_disk("preprocessed_dataset/whisper_prepocessed_dataset.arrow")
-print(f"Загружено {len(dataset)} примеров")
-
-feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-tiny")
-tokenizer = WhisperTokenizer.from_pretrained(
-    "openai/whisper-tiny",
+feature_extractor = WhisperFeatureExtractor.from_pretrained(
+    WHISPER_MODEL_NAME,
     language="russian",
     task="transcribe",
+    predict_timestamps=False,
+    return_attention_mask=True
+)
+tokenizer = PreTrainedTokenizerFast(
+    tokenizer_file="bpe_tokenizer.json",
+    unk_token="<unk>",
+    pad_token="<pad>",
+    bos_token="<s>",
+    eos_token="</s>",
 )
 
+dataset = load_from_disk(DATASET_PATH)
+print(f"Загружено {len(dataset)} примеров")
+
+dataset_builder = DatasetFeatureExtractor(
+    feature_extractor=feature_extractor,
+    tokenizer=tokenizer,
+)
+
+prepared_dataset = dataset_builder.apply(dataset, num_proc=None)
+
+kda_config = create_kda_config(tokenizer=tokenizer)
+
 model = WhisperKDAModel(
-    kda_config=create_kda_config(tokenizer),
-    whisper_model_name="openai/whisper-tiny",
-    freeze_encoder=True).to(device)
+    kda_config=kda_config,
+    whisper_model_name=WHISPER_MODEL_NAME,
+    freeze_encoder=True
+).to(device)
+
+trainable_params = [
+    param
+    for param in model.parameters()
+    if param.requires_grad
+]
 
 optimizer = Lion(
-    model.parameters(),
+    trainable_params,
     lr=3e-5,
     betas=(0.9, 0.99),
     weight_decay=0.1,
 )
 
-dataset_builder = DatasetFeatureExtractor(
-    feature_extractor=feature_extractor,
-    tokenizer=tokenizer
-)
-
-dataset = dataset_builder.apply(dataset)
-
 metrics = Metrics(tokenizer)
+
 data_collator = DataCollator(
     feature_extractor=feature_extractor,
     tokenizer=tokenizer,
@@ -49,9 +70,10 @@ data_collator = DataCollator(
 trainer = Trainer(
     model=model,
     optimizer=optimizer,
-    dataset=dataset,
+    dataset=prepared_dataset,
     data_collator=data_collator,
     metrics=metrics,
     tokenizer=tokenizer,
 )
+
 trainer.train()
